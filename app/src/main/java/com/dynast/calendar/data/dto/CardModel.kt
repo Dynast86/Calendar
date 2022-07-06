50823e743552c9d9c45283504db8643e37416040
“CGKIM” <cgkim@eduwill.net>
2022-07-06 13:25:36 +0900

    Parcelize 추가

--- app/src/main/java/com/dynast/calendar/data/dto/CardModel.kt
+++ app/src/main/java/com/dynast/calendar/data/dto/CardModel.kt
@@ -1,10 +1,13 @@
 package com.dynast.calendar.data.dto
 
+import android.os.Parcelable
 import androidx.compose.runtime.Immutable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 @Immutable
 data class CardModel(
     val title: String,
     val content: String,
     val imageUrl: String? = null
-)
+) : Parcelable
--- app/src/main/java/com/dynast/calendar/domain/model/card/AgendaCardData.kt
+++ app/src/main/java/com/dynast/calendar/domain/model/card/AgendaCardData.kt
@@ -1,7 +1,11 @@
 package com.dynast.calendar.domain.model.card
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class AgendaCardData(
     val title: String,
     val content: String,
     val imageUrl: String? = null
-)
+) : Parcelable
