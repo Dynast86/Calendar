f43cc5bce629be8b66b985570d46b91b058ecdd8
“CGKIM” <cgkim@eduwill.net>
2022-07-07 15:39:08 +0900

    material3 버전업
    1.0.0-alpha13 -> 1.0.0-alpha14

--- app/src/main/java/com/dynast/calendar/domain/model/card/AgendaCardData.kt
+++ app/src/main/java/com/dynast/calendar/domain/model/card/AgendaCardData.kt
@@ -1,9 +1,11 @@
 package com.dynast.calendar.domain.model.card
 
 import android.os.Parcelable
+import androidx.compose.runtime.Immutable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Immutable
 data class AgendaCardData(
     val title: String,
     val content: String,
